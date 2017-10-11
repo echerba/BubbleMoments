6c4eee15e71ff2efa6810e55d237fa602bff3179
Eric Cherba <echerba@gmail.com>
2017-10-10 23:12:37 -0700

    Adding more details to the project. Includng float. and loading textures.

---
+++ Assets/Bubble Moments/Scripts/LoadMoment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadMoment : MonoBehaviour {
+
+    public string url = "https://docs.unity3d.com/uploads/Main/ShadowIntro.png";
+
+    // Use this for initialization
+    IEnumerator Start () {
+        Texture2D tex;
+        tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+        WWW www = new WWW(url);
+        yield return www;
+        www.LoadImageIntoTexture(tex);
+        GetComponent<Renderer>().material.mainTexture = tex;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
